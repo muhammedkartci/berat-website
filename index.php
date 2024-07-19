d0217b97b3f6da73410bad47d280f686329ef83d
muhammedkartci <muhammed.kartci@gmail.com>
2024-07-19 21:35:51 +0300

    Site başlatıldı

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Beratcan Taşlı | Website</title>
+</head>
+
+<body>
+    <div class="header">
+        <div class="logo">
+            <img src="images/berat.png" alt="">
+        </div>
+        <div class="header-menu">
+            <a href="index.php">Anasayfa</a>
+            <a href="hakkimda.php">Hakkımda</a>
+            <a href="odullerim.php">Ödüllerim</a>
+            <a href="iletisim.php">İletişim</a>
+            <a href="fotolar.php">Fotoğraflar</a>
+        </div>
+        <div class="header-search">
+            <img src="images/search.png" alt="">
+        </div>
+    </div>
+    <div class="slider">
+
+        <div class="slider-left">
+            <p class="title">Web ve Sosyal Medyada Başarının Anahtarı</p>
+            <p class="text">Merhaba ve hoş geldiniz! Biz, dijital dünyanın karmaşıklığını sizin için basitleştiren ve
+                işinizi en üst
+                seviyeye taşıyan profesyonel bir dijital hizmet sağlayıcısıyız. Antalya'da bulunan firmamız, web ve
+                sosyal medya yönetimi konusunda uzmanlaşmış ekibiyle size en iyi çözümleri sunmayı hedefliyor.</p>
+            <a class="button" href="">İletişime Geç</a>
+        </div>
+        <div class="slider-right">
+            <img src="images/Muhammed Kartcı - HomeBannerImage.png" alt="">
+        </div>
+    </div>
+</body>
+
+</html>
